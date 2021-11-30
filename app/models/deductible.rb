2ab5a14fba5c92aabc4bc426e7f622f1aa0e4716
eroiyo <eroiyo@outlook.es>
2021-11-30 05:20:12 -0400

    more than one categorie for deductible

--- app/models/deductible.rb
+++ app/models/deductible.rb
@@ -1,6 +1,6 @@
 class Deductible < ApplicationRecord
     belongs_to :user
-    belongs_to :category
+    belongs_to :categories
     has_one_attached :icon
     validates :icon, presence: true
     validates :amount, numericality: { greater_than_or_equal_to: 0 }
