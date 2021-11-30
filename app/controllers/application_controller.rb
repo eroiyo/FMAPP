8cdbafa5d1b268e36f50a65c9adc21ea368d60ac
eroiyo <eroiyo@outlook.es>
2021-11-30 07:58:27 -0400

    erase extra controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :authenticate_user!
+
+    protected
+
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+    end
 end
