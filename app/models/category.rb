0b8f6de687831a62815248374e772cb75daa8690
eroiyo <eroiyo@outlook.es>
2021-12-02 04:44:11 -0400

    reinstall webpacker.yml

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ApplicationRecord
     validates :name, presence: true, length: { maximum: 100 }
     validates :icon, presence: true
-  
+
+    has_one_attached :icon
     belongs_to :user
     has_and_belongs_to_many :deals
   
--- app/models/deductible.rb
+++ app/models/deductible.rb
@@ -3,7 +3,6 @@ class Deductible < ApplicationRecord
     validates :name, :amount, :categories, presence: true, length: { maximum: 100 }
     validates :amount, numericality: { greater_than: 0 }
   
-    has_one_attached :icon
     belongs_to :author, class_name: 'User'
     has_and_belongs_to_many :categories
 end
