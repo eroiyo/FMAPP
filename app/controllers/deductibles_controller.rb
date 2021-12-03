a1873b6c0b954da3220b3785d19dfa12f63c62fe
eroiyo <eroiyo@outlook.es>
2021-12-02 20:23:58 -0400

    pogrest in deductible creation

--- app/controllers/deductibles_controller.rb
+++ app/controllers/deductibles_controller.rb
@@ -6,9 +6,30 @@ class DeductiblesController < ApplicationController
   end
   
   def creation
+    @deductible = Deductible.new
   end
 
   def create
+    @deductible = Deductible.new
+    user = current_user
+    @deductible.user_id = user.id
+    temp = params[:deductible]
+    name = params[:name]
+    amount = params[:amount]
+    
+    picks = params[:picks]
+    picks.each do |category|
+      @deductible.categories << category
+    end
+    @deductible.name = name
+    @deductible.amount = amount
+
+    if @deductible.save
+      redirect_to("/categories/")
+    else
+      flash.alert = "one of the fields is invalid"
+      redirect_to("/deductibles/create")
+    end
   end
 
   def destroy
