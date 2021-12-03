9e03bd918d04de43f7056a327657df6ccb4a102a
eroiyo <eroiyo@outlook.es>
2021-12-03 19:30:46 -0400

    delete confirmation need

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :categories
   has_many :transactions
 end
